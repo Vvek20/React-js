a811eb7339cbc304332c570193a1b7a46f9df6fd
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-02-05 10:57:41 +0000

    afdd

---
+++ vite-project/rounter_06/project02/Routeproject01/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Contact from './Contact'
+import Blogs from './Blogs'
+import NoPage from './NoPage'
+import Layout from './Layout'
+
+function App() {
+
+  return (
+    <>
+   
+    <h1>HELLO_SIR</h1>
+    
+    </>
+    
+   
+  )
+}
+
+export default App
