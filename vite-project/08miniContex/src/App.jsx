66adf60a86a5ba128143cc2e24a0c0d72ad69f34
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-02-06 10:18:13 +0000

    add

--- vite-project/08miniContex/src/App.jsx
+++ vite-project/08miniContex/src/App.jsx
@@ -1,18 +1,18 @@
+
 import './App.css'
-import UserContextProvider from './Context/UserContextProvider'
+import Login from './components/Login'
 import Profile from './components/Profile'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
   
+
   return (
-    <>
     <UserContextProvider>
-      <h1>helloooo</h1>
-      <Login/>
-      <Profile/>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
-     
-    </>
   )
 }
 
