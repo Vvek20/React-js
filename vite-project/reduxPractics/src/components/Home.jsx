62da72e60ed952dd30a62a5f6c11ad04c2c77d04
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-02-07 15:06:47 +0000

    now add

---
+++ vite-project/reduxPractics/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img srt="https://www.shutterstock.com/image-vector/3d-high-quality-vector-mobile-mockups-2121419597" />
+                </div>
+                <div className="img-wrapper item">
+                    <img srt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
