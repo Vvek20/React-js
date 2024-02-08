6bcca200a6ec16ac33fe72a06881fdbba9b7654e
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-02-08 10:47:58 +0000

    addd

--- vite-project/09themeswitcher/src/components/Card.jsx
+++ vite-project/09themeswitcher/src/components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card() {
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Money heist seasion 4
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -66,7 +66,7 @@ export default function Card() {
                     <span className="text-3xl font-bold text-gray-900 dark:text-white">₹5999</span>
                     <a
                         href="/"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         Add to cart
                     </a>
