8b1d6d618a0acc6af6f2c01d550c4e66ff2a2b9c
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-02-14 15:54:00 +0000

    add

---
+++ e-commerce/src/Components/Assets/data.js
@@ -0,0 +1,27 @@
+let data_product=[
+    {
+        id:1,
+        name:"jhdhfcvh",
+        new_price:50.50,
+        old_price:89.50,
+    },
+    {
+        id:1,
+        name:"jhdhfcvh",
+        new_price:50.50,
+        old_price:89.50,
+    },
+    {
+        id:1,
+        name:"jhdhfcvh",
+        new_price:50.50,
+        old_price:89.50,
+    },
+    {
+        id:1,
+        name:"jhdhfcvh",
+        new_price:50.50,
+        old_price:89.50,
+    },
+]
+export default data_product
--- e-commerce/src/Components/Hero/Hero.jsx
+++ e-commerce/src/Components/Hero/Hero.jsx
@@ -9,26 +9,20 @@ const Hero = () => {
         <div>
           <div className='hero-hand-icon'>
             <p>new</p>
-            <img src="https://unsplash.com/photos/woman-in-black-hijab-and-black-abaya-2eC_-0yRrGU" alt='hero' />
+            <img src="" alt='hero' />
           </div>
           <p>collection</p>
           <p>For everyone</p>
         </div>
         <div className='hero-latest-btn'>
           <div>Latest Collection</div>
-          <img src='' alt=''/>
+          <img src='' alt='' />
         </div>
       </div>
       <div className='hero-right'>
-        <img src='' alt=''/>
+        <img src='e-commerce/public/photos/katuva.avif' alt='' />
       </div>
-
-
     </div>
-
-
-
-
   )
 }
 
