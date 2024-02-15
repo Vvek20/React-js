810d74a85772c1aae98de1e689eae334fe67042a
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-02-15 14:35:59 +0000

    add

--- e-commerce/src/Components/Assets/new_collection.js
+++ e-commerce/src/Components/Assets/new_collection.js
@@ -1,32 +1,32 @@
 import product_1 from '../Assets/product_1.jpg'
-let data_product=[
+let new_collection=[
     {
         id:1,
         name:"jhdhfcvh",
-        
+        image:product_1,
         new_price:50.50,
         old_price:89.50,
     },
     {
         id:2,
         name:"jhdhfcvh",
-        
+        image:product_1,
         new_price:50.50,
         old_price:89.50,
     },
     {
         id:3,
         name:"jhdhfcvh",
-      
+        image:product_1,
         new_price:50.50,
         old_price:89.50,
     },
     {
         id:4,
         name:"Carrylux Large Capacity Croco Pattern Tote Bags( Brown )",
-       // image:product_1,
+        image:product_1,
         new_price:560,
         old_price:1999,
     },
 ]
-export default data_product
+export default new_collection
--- e-commerce/src/Components/Offers/Offers.jsx
+++ e-commerce/src/Components/Offers/Offers.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './Offers.css'
- 
- const Offers = () => {
-   return (
-     <div className='offers'>
-        <div className='offers-left'>
-            <h1>Exclusive</h1>
-            <h1>offers for you</h1>
-            <p>ONLY ON BETS SELLERS PRODUCTS</p>
-            <button>Check Now</button>
+import product_1 from '../Assets/product_1.jpg'
 
-        </div>
-        <div className='offers-right'></div>
-        <img/>
+const Offers = () => {
+  return (
+    <div className='offers'>
+      <div className='offers-left'>
+        <h1>Exclusive</h1>
+        <h1>offers for you</h1>
+        <p>ONLY ON BETS SELLERS PRODUCTS</p>
+        <button>Check Now</button>
+
+      </div>
+      <div className='offers-right'></div>
+      <img src={product_1} alt=''/>
        
      </div>
    )
