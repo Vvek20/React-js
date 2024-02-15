9577a755c7fa593852ab90355044bfd890d6ba27
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-02-15 12:25:30 +0000

    add

--- e-commerce/src/Components/Assets/data.js
+++ e-commerce/src/Components/Assets/data.js
@@ -1,27 +1,32 @@
+
 let data_product=[
     {
         id:1,
         name:"jhdhfcvh",
+       
         new_price:50.50,
         old_price:89.50,
     },
     {
-        id:1,
+        id:2,
         name:"jhdhfcvh",
+        
         new_price:50.50,
         old_price:89.50,
     },
     {
-        id:1,
+        id:3,
         name:"jhdhfcvh",
+       
         new_price:50.50,
         old_price:89.50,
     },
     {
-        id:1,
-        name:"jhdhfcvh",
-        new_price:50.50,
-        old_price:89.50,
+        id:4,
+        name:"Carrylux Large Capacity Croco Pattern Tote Bags( Brown )",
+        
+        new_price:560,
+        old_price:1999,
     },
 ]
 export default data_product
